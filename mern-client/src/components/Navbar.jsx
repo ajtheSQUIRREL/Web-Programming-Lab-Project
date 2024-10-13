d07f0c541aba183a310eebab4df2ed8956d7ef40
hasib41 <hasibulhasan169@gmail.com>
2024-10-13 23:12:15 +0600

    working on Navbar

--- mern-client/src/components/Navbar.jsx
+++ mern-client/src/components/Navbar.jsx
@@ -1,46 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { FaBlog } from "react-icons/fa6";
+import { FaBlog } from 'react-icons/fa6'
 
 const Navbar = () => {
-  const [ isMenuOpen, setIsMenuOpen ] = useState(false)
-  const [ isSticky, setIsSticky ] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isSticky, setIsSticky] = useState(false)
 
-  //toggle menu
-  // const toggleMenu = () => {
-  //   setIsMenuOpen(!isMenuOpen)
-  // }
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if(window.screenY > 100){
-  //       setIsSticky(true);
-  //     }
-  //     else{
-  //       setIsSticky(false);
-  //     }
-  //   }
-  //   window.addEventListener("scroll", handleScroll);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true)
+      } else {
+        setIsSticky(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
 
-  //   return () => {
-  //     window,addEventListener("scroll", handleScroll);
-  //   }
-  // },[])
+    return () => {
+      window, addEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   const navItems = [
-    {Link: "Home", path: "/"},
-    {Link: "About", path: "/about"},
-    {Link: "Shop", path: "/shop"},
-    {Link: "Sell Your Book", path: "/admin/dashboard"},
-    {Link: "Blog", path: "/blog"}
+    { Link: 'Home', path: '/' },
+    { Link: 'About', path: '/about' },
+    { Link: 'Shop', path: '/shop' },
+    { Link: 'Sell Your Book', path: '/admin/dashboard' },
+    { Link: 'Blog', path: '/blog' }
   ]
   return (
-       <header>
-        <nav>
-          <div>
-             <Link to= '/'>Books</Link>
-          </div>
-        </nav>
-       </header>
+    <header>
+      <nav>
+        <div>
+          <Link to='/' className='text-2xl font-bold'>
+            <FaBlog className='inline-block' />
+            Books
+          </Link>
+        </div>
+      </nav>
+    </header>
   )
 }
 
